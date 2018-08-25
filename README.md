303dec637a5a7b26e41ed957d0eb6e551f2e1729
William Bork <williambork33@gmail.com>
2018-08-25 14:31:35 -0500

    Pushing MVP version

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # FriendFinder
+
+A compatibility-based "FriendFinder" application. Surveys users, then compares their answers with those from other users (Pokemon in this case). The app will then display the name and picture of the user with the best overall match. 
+
+
+### Before You Begin
+
+1. Clone the repository.
+
+2. Navigate to root folder and run `npm init` to initialize a package.json.
+
+3. Install these Node.js packages:
+
+    * `npm init`
+    * `npm install`
+    * `npm install express`
+    * `npm install body-parser`
+    * `npm install path`
+
+
+### Running Liri
+
+1. Run you local server.
+
+2. Type `node server` to run application.
+
+3. Open on your browser - `http://localhost:8080/`
+
+4. See which Pokemon is best matched for you.
+
+
+### Known Issues
+
+1. There is a routing issue that is commented out on home.html which corresponds with a commented out route in apiRoutes.js file. This doesn't affect performance.
+
+
+### Creator
+
+* https://github.com/WilliamBork33
+
+* http://www.williambork.com/
+
+
+### Forked From Users:
+* https://github.com/diani091
+
+* https://github.com/ChechesCOM
+
---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+//Objects for each potential friend
+var friendList = [
+    {
+      name: "Bulbasaur",
+      profilePic: "https://upload.wikimedia.org/wikipedia/en/2/28/Pok%C3%A9mon_Bulbasaur_art.png",
+      scores: [
+        "3",
+        "1",
+        "1",
+        "5",
+        "3",
+        "1",
+        "3",
+        "2",
+        "3",
+        "5"
+      ]
+    },
+    {
+        name: "Charmander",
+        profilePic: "https://upload.wikimedia.org/wikipedia/en/a/a5/Pok%C3%A9mon_Charmander_art.png",
+        scores: [
+          "2",
+          "2",
+          "2",
+          "2",
+          "2",
+          "2",
+          "3",
+          "1",
+          "2",
+          "2"
+        ]
+      },
+
+      {
+        name: "Squirtle",
+        profilePic: "https://upload.wikimedia.org/wikipedia/en/5/59/Pok%C3%A9mon_Squirtle_art.png",
+        scores: [
+          "5",
+          "2",
+          "5",
+          "1",
+          "1",
+          "5",
+          "2",
+          "4",
+          "5",
+          "5"
+        ]
+      },
+
+      {
+        name: "Pikachu",
+        profilePic: "https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png",
+        scores: [
+          "1",
+          "5",
+          "2",
+          "4",
+          "3",
+          "2",
+          "4",
+          "1",
+          "5",
+          "5"
+        ]
+      }
+  ]
+
+module.exports = friendList;
+ 
